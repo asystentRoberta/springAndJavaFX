0febd532e3ee5794f1dec2ceaf06b46a395d46d9
AsystentRoberta <asystent.roberta@gmail.com>
2020-03-02 00:45:23 +0100

    Prepare SpringBootApplication

---
+++ src/main/java/pl/com/bohdziewicz/javaFxAndSpring/App.java
@@ -0,0 +1,27 @@
+package pl.com.bohdziewicz.javaFxAndSpring;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(ApplicationContext applicationContext) {
+
+        return new CommandLineRunner() {
+            @Override public void run(String... args) throws Exception {
+
+                System.out.println("Hi!");
+            }
+        };
+    }
+}
